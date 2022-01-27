b58d8901aae4e7964fd7725eda0404445f9a8c1b
shaharKamay <shahar.kamay@gmail.com>
2022-01-27 13:18:54 +0200

    changed lambda to typescript

--- lib/aws-cdk-cymotive-stack.ts
+++ lib/aws-cdk-cymotive-stack.ts
@@ -6,11 +6,14 @@ import {
   aws_lambda,
   Duration,
   aws_dynamodb,
-  aws_s3_notifications,
   aws_lambda_event_sources,
+  aws_sqs,
+  aws_iam,
+  aws_lambda_nodejs,
 } from 'aws-cdk-lib';
+import { IntegrationOptions } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class AwsCdkCymotiveStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,29 +23,110 @@ export class AwsCdkCymotiveStack extends Stack {
       bucketName: 'reports34567890844',
     });
 
-    const porter = new aws_lambda.Function(this, 'porter2', {
-      runtime: aws_lambda.Runtime.NODEJS_14_X,
-      code: aws_lambda.Code.fromAsset('functions'),
-      handler: 'porter.handler',
+    const porterRole = new aws_iam.Role(this, 'PorterRole', {
+      assumedBy: new aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+      roleName: 'PorterRole',
+      description: 'Role for porter lambda',
+      inlinePolicies: {
+        PorterPolicy: new aws_iam.PolicyDocument({
+          statements: [
+            new aws_iam.PolicyStatement({
+              effect: aws_iam.Effect.ALLOW,
+              actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents',
+              ],
+              resources: [
+                `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/lambda/*:*:*`,
+              ],
+            }),
+            new aws_iam.PolicyStatement({
+              effect: aws_iam.Effect.ALLOW,
+              actions: ['s3:PutObject'],
+              resources: [bucket.bucketArn],
+            }),
+          ],
+        }),
+      },
+    });
+
+    // const porter = new aws_lambda.Function(this, 'porter2', {
+    //   runtime: aws_lambda.Runtime.NODEJS_14_X,
+    //   code: aws_lambda.Code.fromAsset('functions'),
+    //   handler: 'porter.handler',
+    //   functionName: 'porter2',
+    //   timeout: Duration.seconds(8),
+    //   role: porterRole,
+    // });
+
+    const porter = new aws_lambda_nodejs.NodejsFunction(this, 'porter2', {
+      entry: join(__dirname, '..', 'functions', 'porter.ts'),
+      handler: 'handler',
       functionName: 'porter2',
       timeout: Duration.seconds(8),
+      role: porterRole,
     });
 
     bucket.grantWrite(porter);
 
     const api = new aws_apigateway.RestApi(this, 'cymotive-reports', {
+      deployOptions: {
+        stageName: 'api',
+      },
       restApiName: 'cymotive-reports',
       endpointTypes: [aws_apigateway.EndpointType.REGIONAL],
     });
 
-    const writeReportsIntegration = new aws_apigateway.LambdaIntegration(
-      porter,
-      {
-        requestTemplates: { 'application/json': '{ "statusCode": "200" }' },
-      }
+    const recordsQueue = new aws_sqs.Queue(this, 'recordsSqsQueue', {
+      queueName: 'recordsQueue',
+    });
+    // porter.addEventSource(
+    //   new aws_lambda_event_sources.SqsEventSource(queue, {
+    //     batchSize: 10,
+    //   })
+    // );
+
+    const idsGatewayRole = new aws_iam.Role(this, 'IdsGatewayRole', {
+      assumedBy: new aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+
+    const methodOptions: IntegrationOptions = {
+      credentialsRole: idsGatewayRole,
+      passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
+      requestParameters: {
+        'integration.request.header.Content-Type':
+          "'application/x-www-form-urlencoded'",
+      },
+      requestTemplates: {
+        'application/json':
+          'Action=SendMessage&QueueUrl=$util.urlEncode("' +
+          recordsQueue.queueUrl +
+          '")&MessageBody=$util.urlEncode($input.body)', //Request body
+      },
+      integrationResponses: [{ statusCode: '200' }],
+    };
+
+    api.root.addMethod(
+      'POST',
+      new aws_apigateway.AwsIntegration({
+        service: 'sqs',
+        region: this.region,
+        path: `${this.account}/${recordsQueue.queueName}`,
+        integrationHttpMethod: 'POST',
+        options: methodOptions,
+      }),
+      { methodResponses: [{ statusCode: '200' }] }
     );
 
-    api.root.addMethod('POST', writeReportsIntegration);
+    porter.addEventSource(
+      new aws_lambda_event_sources.SqsEventSource(recordsQueue, {
+        enabled: true,
+      })
+    );
+
+    recordsQueue.grantSendMessages(idsGatewayRole);
+    recordsQueue.grantConsumeMessages(porterRole);
 
     const table = new aws_dynamodb.Table(this, 'ids-table2', {
       tableName: 'ids-table2',
@@ -52,13 +136,21 @@ export class AwsCdkCymotiveStack extends Stack {
       },
     });
 
-    const ingest = new aws_lambda.Function(this, 'ingest2', {
-      runtime: aws_lambda.Runtime.NODEJS_14_X,
-      code: aws_lambda.Code.fromAsset('functions'),
-      handler: 'ingest.handler',
+    // const ingest = new aws_lambda.Function(this, 'ingest2', {
+    //   runtime: aws_lambda.Runtime.NODEJS_14_X,
+    //   code: aws_lambda.Code.fromAsset('functions'),
+    //   handler: 'ingest.handler',
+    //   functionName: 'ingest2',
+    //   timeout: Duration.seconds(8),
+    // });
+
+    const ingest = new aws_lambda_nodejs.NodejsFunction(this, 'ingest2', {
+      entry: join(__dirname, '..', 'functions', 'ingest.ts'),
+      handler: 'handler',
       functionName: 'ingest2',
       timeout: Duration.seconds(8),
     });
+
     bucket.grantRead(ingest);
     table.grantWriteData(ingest);
 
@@ -70,15 +162,21 @@ export class AwsCdkCymotiveStack extends Stack {
     );
     ingest.addEventSource(s3PutEventSource);
 
-    const analyzer = new aws_lambda.Function(this, 'analyzer2', {
-      runtime: aws_lambda.Runtime.NODEJS_14_X,
-      code: aws_lambda.Code.fromAsset('functions'),
-      handler: 'analyzer.handler',
+    // const analyzer = new aws_lambda.Function(this, 'analyzer2', {
+    //   runtime: aws_lambda.Runtime.NODEJS_14_X,
+    //   code: aws_lambda.Code.fromAsset('functions'),
+    //   handler: 'analyzer.handler',
+    //   functionName: 'analyzer2',
+    //   timeout: Duration.seconds(8),
+    // });
+
+    const analyzer = new aws_lambda_nodejs.NodejsFunction(this, 'analyzer2', {
+      entry: join(__dirname, '..', 'functions', 'analyzer.ts'),
+      handler: 'handler',
       functionName: 'analyzer2',
       timeout: Duration.seconds(8),
     });
 
-    // table.grantReadData(analyzer);
     table.grantFullAccess(analyzer);
 
     const countInfo = new aws_apigateway.LambdaIntegration(analyzer, {
@@ -86,10 +184,5 @@ export class AwsCdkCymotiveStack extends Stack {
     });
 
     api.root.addMethod('GET', countInfo);
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkCymotiveQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
